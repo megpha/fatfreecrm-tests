e3372a39ef094327deb578948e44bb499bbd113f
arjun <iarjun@arjuns-MacBook-Pro.local>
2022-05-04 22:35:25 -0400

    adding tests

--- src/test/java/com/learning/LoginPageTests.java
+++ src/test/java/com/learning/LoginPageTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.learning.pages.LoginPage;
@@ -12,20 +13,17 @@ public class LoginPageTests extends TestNgTestBase {
 
     private LoginPage loginPage;
 
-    @BeforeMethod
-    public void gotoLoginPage() {
-        driver.get(baseUrl);
-        loginPage = new LoginPage(driver);
-    }
-
-
     @Test
     public void testTitle() {
+        driver.get(baseUrl);
+        loginPage = new LoginPage(driver);
         Assert.assertEquals("Facebook - log in or sign up", loginPage.getTitle());
     }
 
     @Test
     public void testLoginWithNoPassword() {
+        driver.get(baseUrl);
+        loginPage = new LoginPage(driver);
         loginPage.login("username", "password");
         Assert.assertEquals("Facebook - log in or sign up", loginPage.getTitle());
     }
